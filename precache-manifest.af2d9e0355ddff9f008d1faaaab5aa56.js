dda5dc1ea363110e57c3ec0740908780ab1e3cfb
Gates Paek <kxl32551@gmail.com>
2019-11-18 23:57:57 +0900

    Updates

---
+++ precache-manifest.af2d9e0355ddff9f008d1faaaab5aa56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7670880a7b2ec2218c93decd5e8ba811",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "78849119fafd927a8ebf",
+    "url": "/movie_app_2019/static/css/main.68c30a09.chunk.css"
+  },
+  {
+    "revision": "4e3d0de9869208895014",
+    "url": "/movie_app_2019/static/js/2.5b209c19.chunk.js"
+  },
+  {
+    "revision": "78849119fafd927a8ebf",
+    "url": "/movie_app_2019/static/js/main.98f20a53.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
